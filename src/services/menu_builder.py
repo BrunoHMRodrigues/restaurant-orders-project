b7620f82ad97b0438f65448449ceec12fe6c98cf
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-07-20 23:15:58 -0300

    Feature: Requisite 04

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,16 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        main_menu = []
+        for dish in self.menu_data.dishes:
+            # if restriction not in dish.get_ingredients().restrictions:
+            if not any(restriction in ingredient.restrictions for ingredient in dish.get_ingredients()):
+                dish_info = {
+                    "dish_name": dish.name,
+                    "ingredients": dish.get_ingredients(),
+                    "price": dish.price,
+                    "restrictions": dish.get_restrictions()
+                }
+                main_menu.append(dish_info)
+        return main_menu
+
